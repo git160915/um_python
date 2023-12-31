71aae0bace69bea41936a8591b053aba40cdf9c7
Gia Truong - W <gntgit160915@gmail.com>
2023-12-31 22:06:05 +1100

    Completed Capstone Week 6 assignment.

---
+++ capstone/week06/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','cam.ac.uk','mac.com','indiana.edu','berkeley.edu','uct.ac.za','unicon.net','ucdavis.edu','gmail.com','yale.edu'],
+['2005',57,7,12,12,12,14,6,11,10,5],
+['2006',742,335,310,309,248,238,218,193,164,140]
+];
